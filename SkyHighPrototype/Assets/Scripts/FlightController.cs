b0bcb0020e12db4ca8a086bdcef3da8f7c461b42
3b0ziz <abdulaziz.alioglu007@gmail.com>
2026-04-01 23:38:43 +0300

    feat: implement pitch and yaw rotation

---
+++ SkyHighPrototype/Assets/Scripts/FlightController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+// Simple aircraft controller for HW1
+public class FlightController : MonoBehaviour
+{
+    [SerializeField] private float pitchSpeed = 45f;
+    [SerializeField] private float yawSpeed = 45f;
+    [SerializeField] private float rollSpeed = 45f;
+    [SerializeField] private float thrustSpeed = 5f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        HandleRotation();
+        HandleThrust();
+    }
+
+    private void HandleRotation()
+    {
+        float pitch = Input.GetAxis("Vertical") * pitchSpeed * Time.deltaTime;
+        float yaw = Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+
+        transform.Rotate(Vector3.right * pitch);
+        transform.Rotate(Vector3.up * yaw);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.forward * rollSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(-Vector3.forward * rollSpeed * Time.deltaTime);
+        }
+    }
+
+    private void HandleThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.forward * thrustSpeed * Time.deltaTime);
+        }
+    }
+}
