5fdaa19c77cda43a8651533431d7ca9b75fcf7e1
3b0ziz <abdulaziz.alioglu007@gmail.com>
2026-04-02 17:45:13 +0300

    feat: complete landing system with pass/fail logic

--- SkyHighPrototype/Assets/Scripts/LandingZoneController.cs
+++ SkyHighPrototype/Assets/Scripts/LandingZoneController.cs
@@ -4,15 +4,33 @@ public class LandingZoneController : MonoBehaviour
 {
     [SerializeField] private FlightExamManager examManager;
     [SerializeField] private AudioSource landingAudioSource;
+    [SerializeField] private float maxLandingSpeed = 10f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (landingAudioSource != null)
-                landingAudioSource.Play();
-                
-            examManager.CompleteMission();
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                float speed = rb.linearVelocity.magnitude;
+
+                if (speed <= maxLandingSpeed)
+                {
+                    Debug.Log("Successful Landing!");
+
+                    if (landingAudioSource != null)
+                        landingAudioSource.Play();
+
+                    examManager.PassExam();
+                }
+                else
+                {
+                    Debug.Log("Crash Landing!");
+                    examManager.FailExam();
+                }
+            }
         }
     }
 }
